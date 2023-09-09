2ad7a10627fb82699e8f8574caafbb8cafc64544
03li <moh.cvcv99@gmail.com>
2023-09-09 16:08:20 +0100

    set authorities base authentication

--- src/main/java/com/example/demo/security/Role.java
+++ src/main/java/com/example/demo/security/Role.java
@@ -4,6 +4,9 @@ import static com.example.demo.security.Permission.*;
 
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public enum Role {
     STUDENT(
@@ -16,7 +19,14 @@ public enum Role {
             STUDENT_READ,
             STUDENT_WRITE
         )
-    );
+    ),
+    ADMINTRAINEE(
+        Set.of(
+            COURSE_READ,
+            STUDENT_READ
+        )
+    )
+    ;
 
     private final Set<Permission> permissions;
 
@@ -25,6 +35,14 @@ public enum Role {
     }
 
     public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
